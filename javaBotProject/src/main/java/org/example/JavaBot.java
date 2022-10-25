ff7328dbc98783215e77acf4595b00f92f37dc90
Giuseppe Bonanno <83579728+GiuseppeBnn@users.noreply.github.com>
2022-10-25 18:42:36 +0200

    Piccoli miglioramenti
    Risolto ripetizione /start

--- javaBotProject/src/main/java/org/example/JavaBot.java
+++ javaBotProject/src/main/java/org/example/JavaBot.java
@@ -1,8 +1,10 @@
 package org.example;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class JavaBot extends TelegramLongPollingBot {
     private final String Token="5699816726:AAEzmF2NhHg4CwhYRuc7XjPgLahx0Af8-Ig";
@@ -15,9 +17,35 @@ public class JavaBot extends TelegramLongPollingBot {
     @Override
     public void onUpdateReceived(Update update) {
         if(update.hasMessage()&&update.getMessage().hasText()){
-            String messaggio= update.getMessage().getText();
-            System.out.println(messaggio);
-            
+            int caso=0;
+
+            String messaggioRicevuto= update.getMessage().getText();
+            System.out.println(messaggioRicevuto);
+            String chatId= update.getMessage().getChatId().toString();
+            System.out.println(chatId);
+
+            SendMessage sendMessage=new SendMessage();
+            sendMessage.setChatId(update.getMessage().getChatId().toString());
+
+            if(messaggioRicevuto.equals("/start")){
+                caso=1;
+            }
+                switch (caso){
+                    case 1:                                       //caso /start
+                            sendMessage.setText("Benvenuto su JavaBot! Questo è un piccolo EchoBot, ossia ripete i messaggi ricevuti.");
+                            try{
+                                execute(sendMessage);
+                            }catch(TelegramApiException e){ e.printStackTrace();}
+                            sendMessage.setText("Scrivi qualsiasi cosa...");
+                            try{
+                                execute(sendMessage);
+                            }catch(TelegramApiException e){ e.printStackTrace();}   break;
+                    default:
+                            sendMessage.setText(messaggioRicevuto);
+                            try{
+                               execute(sendMessage);
+                            }catch(TelegramApiException e){ e.printStackTrace();}  break;
+                }
         }
     }
 
